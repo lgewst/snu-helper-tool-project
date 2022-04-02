829c46153abf2ae8d27f93e585cf30729fb21392
pasa3232 <whtmdgks1120@gmail.com>
2022-04-02 16:05:03 +0900

    add custom error

--- backend/sync-helper/config/settings.py
+++ backend/sync-helper/config/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'config.error.custom_exception_handler'
+}
