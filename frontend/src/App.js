680291f511b1c44cef33cced61d7764dd7838f65
hursuhyun <hursuhyun@csap.snu.ac.kr>
2022-04-04 18:00:14 +0900

    added init page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,31 @@ import Foldername from './Components/FolderName/FolderName';
 import Path from './Components/Path/Path';
 import ConflictInfo from './Components/ConflictInfo/ConflictInfo';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import InitPage from './Containers/InitPage/InitPage';
+
 function App() {
+  const init = false;
+
   return (
     <div className="App">
-      <Path></Path>
-      <Foldername></Foldername>
-      <ConflictInfo></ConflictInfo>
+      {init ? (
+        <div>
+          <Path></Path>
+          <Foldername></Foldername>
+          <ConflictInfo></ConflictInfo>
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/init"
+              exact
+              render={() => <InitPage title="InitPage" />}
+            />
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
---
+++ frontend/src/Containers/InitPage/InitPage.js
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const InitPage = (props) => {
+  const history = useHistory();
+  const [initState, setinitState] = useState({
+    chromium_repo: '',
+    webosose_repo: '',
+    current_version: '',
+    target_version: '',
+  });
+
+  const { chromium_repo, webosose_repo, current_version, target_version } =
+    initState;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setinitState({
+      ...initState,
+      [name]: value,
+    });
+    console.log(initState);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(initState);
+    const response = axios
+      .get('/chromium/init', { params: initState })
+      .then((res) => {
+        alert(res.data.message);
+        // TODO
+        // store data in somewhere
+        history.push('/dir');
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+
+    if (response.status === 200) {
+      console.log(response.data.message);
+      alert(response.data.message);
+      history.push('/dir');
+    } else {
+      alert(response.data.message);
+    }
+  };
+
+  const init = async () => {};
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="chromium">chromium repo path: </label>
+      <input
+        type="text"
+        id="chromium"
+        name="chromium_repo"
+        placeholder="ex. /home/seunghan/chromium/src"
+        size="50"
+        onChange={onChange}
+        value={chromium_repo}
+      ></input>
+      <br />
+      <label htmlFor="webosose">webosose repo path: </label>
+      <input
+        type="text"
+        id="webosose"
+        name="webosose_repo"
+        placeholder="ex. /home/seunghan/chromium91"
+        size="50"
+        onChange={onChange}
+        value={webosose_repo}
+      ></input>
+      <br />
+      <label htmlFor="cur_ver">current version: </label>
+      <input
+        type="text"
+        id="cur_ver"
+        name="current_version"
+        placeholder="ex. 91.0.4472.0"
+        size="50"
+        onChange={onChange}
+        value={current_version}
+      ></input>
+      <br />
+      <label htmlFor="tar_ver">target version: </label>
+      <input
+        type="text"
+        id="tar_ver"
+        name="target_version"
+        placeholder="ex. 92.0.4515.0"
+        size="50"
+        onChange={onChange}
+        value={target_version}
+      ></input>
+      <br />
+      <br />
+      <input
+        type="submit"
+        value="Submit"
+        disabled={
+          !(
+            initState.webosose_repo &&
+            initState.chromium_repo &&
+            initState.target_version &&
+            initState.current_version
+          )
+        }
+      />
+    </form>
+  );
+};
+
+export default InitPage;
