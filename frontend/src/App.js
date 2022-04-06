2bf3579825717b594a98c01699d9d195420e2e61
hursuhyun <hursuhyun@csap.snu.ac.kr>
2022-04-06 12:20:40 +0900

    changing app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,39 @@ import './App.css';
 import Foldername from './Components/FolderName/FolderName';
 import Path from './Components/Path/Path';
 import ConflictInfo from './Components/ConflictInfo/ConflictInfo';
+import FolderPage from './Containers/FolderPage/FolderPage';
+import FilePage from './Containers/FilePage/FilePage';
+import ErrorPage from './Containers/ErrorPage/ErrorPage';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import InitPage from './Containers/InitPage/InitPage';
+import { useState } from 'react';
 
 function App() {
-  const init = false;
+  const [initialized, setinit] = useState(true);
+  console.log('init', initialized);
 
   return (
     <div className="App">
-      {init ? (
+      {initialized ? (
         <div>
-          <Path></Path>
-          <Foldername></Foldername>
-          <ConflictInfo></ConflictInfo>
+          <BrowserRouter>
+            <Switch>
+              <Route
+                path="/path"
+                render={() => <FolderPage title="FolderPage" />}
+              />
+              <Route
+                path="/file"
+                render={() => <FilePage title="FilePage" />}
+              />
+              <Route
+                path="/error"
+                render={() => <ErrorPage title="ErrorPage" />}
+              />
+              <Redirect from="/" to="/path" />
+            </Switch>
+          </BrowserRouter>
         </div>
       ) : (
         <BrowserRouter>
@@ -24,7 +43,10 @@ function App() {
               path="/init"
               exact
               render={() => <InitPage title="InitPage" />}
+              initialized={initialized}
+              setinit={setinit}
             />
+            <Redirect from="/" to="/init" />
           </Switch>
         </BrowserRouter>
       )}
