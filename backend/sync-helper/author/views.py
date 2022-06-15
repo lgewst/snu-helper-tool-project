80da5c51b252180214a0262c06f1c7b4d7942fde
pasa3232 <whtmdgks1120@gmail.com>
2022-06-15 23:47:21 +0900

    add /author/url/

--- backend/sync-helper/author/views.py
+++ backend/sync-helper/author/views.py
@@ -7,6 +7,7 @@ from os import scandir, path, sep
 from subprocess import PIPE, Popen
 
 from chromium.models import *
+from chromium.crawling import *
 from config.error import *
 
 # Create your views here.
@@ -17,6 +18,16 @@ class AuthorViewSet(viewsets.GenericViewSet):
     def url(self, request):
         if not Chromium.INITIALIZED:
             raise InitializeException()
+        
+        commit_id = request.query_params.get('commit_id')
+        if commit_id is None:
+            return Response({"message": "Send 'commit_id'"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        author_email = request.query_params.get('author_email')
+        if author_email is None:
+            return Response({"message": "Send 'author_email'"}, status=status.HTTP_400_BAD_REQUEST)
+
+        find_index(commit_id, author_email);
 
     # GET /author/related
     @action(detail=False, methods=['GET'], url_path='url')
